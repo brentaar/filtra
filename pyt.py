7d653127173029380e74cb1502e6170a91433dd5
Archie Brentano <brentaar@gmail.com>
2011-12-30 18:19:18 +0000

    added function for better encapsulation

--- pyt.py
+++ pyt.py
@@ -8,6 +8,17 @@ import hashlib
 
 con = None
 
+def fileindex():
+  pytcwd = os.getcwd()
+ 
+  for root,dirs,files in os.walk(pytcwd):
+    for files in files:
+      filespli = os.path.splitext(files)
+      fileex = filespli[1]
+      filepath = os.path.join(root,files)
+      filename = os.path.basename(files)
+      cur.execute("INSERT INTO pyth_files SET filename = '%s', filepath = '%s', filetype = '%s', fileexist = 1 " % (filename, filepath,fileex))    
+
 try:
 
  con = mdb.connect('localhost', 'pyth', 'shafted', 'pyth');
@@ -19,23 +30,15 @@ try:
  
  print "Database version : %s " % data
  
- 
- pytcwd = os.getcwd()
- 
- for root,dirs,files in os.walk(pytcwd):
-  for files in files:
-   filespli = os.path.splitext(files)
-   fileex = filespli[1]
-   filepath = os.path.join(root,files)   
-   filename = os.path.basename(files)
-   cur.execute("INSERT INTO pyth_files SET filename = '%s', filepath = '%s', filetype = '%s', fileexist = 1 " % (filename, filepath,fileex))    
-
 except mdb.Error, e:
 
  print "Error %d: %s" % (e.args[0],e.args[1])
  sys.exit(1)
 
-finally:    
+finally:
+  fileindex()
 
- if con:    
-  con.close()
+  if con:    
+    con.close()
+
+
