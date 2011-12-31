c1b8180b9e36fe0702585d489734644967587506
Archie Brentano <ser@ser-desktopB.(none)>
2011-12-30 23:36:52 -0800

    basic functionality implemented, still needs help text

--- pyt.py
+++ pyt.py
@@ -9,35 +9,87 @@ import hashlib
 con = None
 
 def fileindex():
-  pytcwd = os.getcwd()
- 
   for root,dirs,files in os.walk(pytcwd):
     for files in files:
       filespli = os.path.splitext(files)
       fileex = filespli[1]
+      fileex = fileex.strip(".")
       filepath = os.path.join(root,files)
       filename = os.path.basename(files)
-      cur.execute("INSERT INTO pyth_files SET filename = '%s', filepath = '%s', filetype = '%s', fileexist = 1 " % (filename, filepath,fileex))    
+      cur.execute("SELECT * FROM pyth_files WHERE filepath = '%s'" % filepath)
+      rows = cur.fetchone()
+      
+      if rows < 1:     
+        cur.execute("INSERT INTO pyth_files SET filename = '%s', filepath = '%s', filetype = '%s', fileexist = 1 " % (filename,filepath,fileex))    
+    con.commit()
+  cur.close()
+
+def filehash():
+  cur.execute("SELECT id,filepath FROM pyth_files WHERE fileexist = 1 AND filepath LIKE '%s%%' " % pytcwd) 
+  rows = cur.fetchall()
+  for row in rows:
+    filehash = filemd5(row[1])
+    #print "fh: %s" % filehash
+    cur.execute("SELECT fileid,hash FROM pyth_hash WHERE fileid = '%s' " % row[0])
+    rows1 = cur.fetchone()
+    if rows1 < 1:
+      cur.execute("INSERT INTO pyth_hash SET fileid = '%s', hash = '%s' " % (row[0],filehash))
+    else:
+      cur.execute("UPDATE pyth_hash SET hashstamp = CURRENT_TIMESTAMP WHERE fileid = '%s' " % row[0])
+    con.commit()
+  cur.close
+  
+def filemd5(filepath):
+ md5 = hashlib.md5()
+ f = open(filepath)
+ while True:
+  data = f.read(8192)
+  if not data:
+    break
+  md5.update(data)
+ return md5.hexdigest()
+  
+def filedupes():
+  print "DUPES"
+  cur.execute("SELECT fileid,hash FROM pyth_hash WHERE hash IN ( SELECT hash FROM pyth_hash GROUP BY hash HAVING count(hash) > 1 ) ORDER BY hash")
+  rows = cur.fetchall()
+  hashnumber = None
+  for row in rows:
+    cur.execute("SELECT filepath from pyth_files WHERE id = '%s' " % row[0])
+    out = cur.fetchone()
+    tmphash = row[1]
+    if hashnumber != tmphash:
+      print "HASH: %s" % row[1]
+      hashnumber = row[1]
+    print " %s " % out
+  
+  
+  ######################
+      
+if(len(sys.argv) < 2):
+  print "help text"
+  sys.exit(1)
 
 try:
-
- con = mdb.connect('localhost', 'pyth', 'shafted', 'pyth');
  
+ con = mdb.connect('localhost', 'pyth', 'shafted', 'pyth') 
  cur = con.cursor()
- cur.execute("SELECT VERSION()")
- 
- data = cur.fetchone()
- 
- print "Database version : %s " % data
- 
+ pytcwd = os.getcwd() 
+ if( sys.argv[1] == 'index' ):
+  fileindex()
+ elif( sys.argv[1] == 'hash' ):
+  filehash()
+ elif( sys.argv[1] == 'dupes' ):
+  filedupes()
+ else:
+  print "help text"
+  
 except mdb.Error, e:
 
  print "Error %d: %s" % (e.args[0],e.args[1])
  sys.exit(1)
 
 finally:
-  fileindex()
-
   if con:    
     con.close()
 
--- pyth_table.sql
+++ pyth_table.sql
@@ -1,14 +1,16 @@
-CREATE TABLE IF NOT EXISTS pyth_files( 
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  filename TEXT,
-  filepath TEXT,
-  filetype VARCHAR(20),
-  fileexist BINARY
-  );
-CREATE TABLE IF NOT EXISTS pyth_hash(
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  fileid BIGINT,
-  hash VARCHAR(50),
-  hashdate DATE
-  );
-  
+CREATE TABLE `pyth_files` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `filename` text,
+  `filepath` text,
+  `filetype` varchar(20) DEFAULT NULL,
+  `fileexist` binary(1) DEFAULT NULL,
+  `filestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `pyth_hash` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `fileid` bigint(20) NOT NULL,
+  `hash` varchar(32) NOT NULL DEFAULT '',
+  `hashstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
