dcf23e4298c6eef2a4101193bfa4291c3efcaf92
Archie Brentano <brentaar@gmail.com>
2012-01-03 07:50:01 -0800

    adding cluesless table updates

--- pyth_table.sql
+++ pyth_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE `pyth_files` (
   `filetype` varchar(20) DEFAULT NULL,
   `fileexist` binary(1) DEFAULT NULL,
   `filestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `filesize`  varchar,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `pyth_hash` (
@@ -13,4 +14,12 @@ CREATE TABLE `pyth_hash` (
   `fileids` text NOT NULL,
   `hashstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `other_table` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `hashid` int NOT NULL ,
+  `fileid` int NOT NULL,
+  primary key(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
