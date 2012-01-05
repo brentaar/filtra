e9d68ea73e7be33186cfbc2d5d2efc7c7c87df96
Archie Brentano <brentaar@gmail.com>
2012-01-05 14:49:17 -0800

    dealing with files not existing even though they are found

--- pyt.py
+++ pyt.py
@@ -16,6 +16,8 @@ def fileindex():
       fileex = filespli[1]
       fileex = fileex.strip(".")
       filepath = os.path.join(root,files)
+      if not os.path.exists(filepath):
+       continue
       filestat = os.stat(filepath)
       filepath = msani(filepath)
       
@@ -111,37 +113,40 @@ def filemd5(filepath,filesize):
   return 0
  if readsize > 8192:
   while True:
-    data = f.read(8192)
-    if not data:
-     break
+   data = f.read(8192)
+   if not data:
+    break
    md5.update(data)
  else:
-   data = f.read(readsize)
-   md5.update(data)
+  data = f.read(readsize)
+  md5.update(data)
  return md5.hexdigest()
 
 def hashclean():
-  q1 = "SELECT id FROM pyth_files WHERE fileexist = 0"
-  cur.execute(q1)
-  rows = cur.fetchall()
-  for row in rows:
-    id = row[0]
-    q2 = """SELECT id,fileids 
-      FROM pyth_hash 
-      WHERE fileids = '%s' 
-      OR fileids LIKE '%%,%s' 
-      OR fileids LIKE '%s,%%' 
-      OR fileids LIKE '%%,%s,%%' """ % (id,id,id,id)
-    cur.execute(q2)
-    q2out = cur.fetchone()
-    q2outsplit = q2out[1].split(",")    
-    q2outsplit.remove(str(id))
-    q2outjoin = ",".join(q2outsplit)
+ q1 = "SELECT id FROM pyth_files WHERE fileexist = 0"
+ cur.execute(q1)
+ rows = cur.fetchall()
+ for row in rows:
+  id = row[0]
+  q2 = """SELECT id,fileids 
+   FROM pyth_hash 
+   WHERE fileids = '%s' 
+   OR fileids LIKE '%%,%s' 
+   OR fileids LIKE '%s,%%' 
+   OR fileids LIKE '%%,%s,%%' """ % (id,id,id,id)
+  cur.execute(q2)
+  q2out = cur.fetchone()
+  if not q2out:
+   continue
+  fileids = q2out[1]
+  q2outsplit = fileids.split(",")    
+  q2outsplit.remove(str(id))
+  q2outjoin = ",".join(q2outsplit)
     
-    q3 = "UPDATE pyth_hash SET fileids = '%s', hashstamp = CURRENT_TIMESTAMP WHERE id = '%s' " % (q2outjoin,q2out[0])
-    print q3
-    cur.execute(q3)
-    con.commit() 
+  q3 = "UPDATE pyth_hash SET fileids = '%s', hashstamp = CURRENT_TIMESTAMP WHERE id = '%s' " % (q2outjoin,q2out[0])
+  print q3
+  cur.execute(q3)
+ con.commit() 
     
 def filedupes():
   q1 = "SELECT fileids,hash FROM pyth_hash WHERE fileids LIKE '%%,%%' "
